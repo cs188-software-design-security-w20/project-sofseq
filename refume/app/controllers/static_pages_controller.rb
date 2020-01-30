6683005b575be1a17ad6c5d49890cc41cc98bbda
wahjay <hotan38@berkeley.edu>
2020-01-30 11:59:30 -0800

    add contact help page

--- refume/app/controllers/static_pages_controller.rb
+++ refume/app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
   protect_from_forgery with: :exception
-  
+
   def home
   end
 
   def about
+  end
+
+  def contact
+  end
+
+  def help
   end
 end
