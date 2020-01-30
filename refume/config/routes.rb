9974e19237f598043b3c362c87d5f1ac8944f367
Arnav Garg <arnavgrg@gmail.com>
2020-01-30 12:03:11 -0800

    Added info view

--- refume/config/routes.rb
+++ refume/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'users/new'
 
+  #static_pages routes
   get 'static_pages/home'
-
   get 'static_pages/about'
+  get 'static_pages/info'
 
+  #user routes
+  get 'users/new'
   get 'users/mentor_mentee'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
