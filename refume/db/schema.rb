41e4f89ab0d0b4745c2c464d1c936fc248991084
jasonmally <jasonmally1@gmail.com>
2020-02-08 22:13:39 -0800

    mally - micropost migration

--- refume/db/schema.rb
+++ refume/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200206193629) do
+ActiveRecord::Schema.define(version: 20200209060154) do
 
   create_table "matches", force: :cascade do |t|
     t.integer "mentor_id"
     t.integer "mentee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
