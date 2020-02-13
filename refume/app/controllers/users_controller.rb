3e3571ec88eda75eb6a7571c4ceed8c3eac9c288
wahjay <hotan38@berkeley.edu>
2020-02-13 11:46:02 -0800

    add logout path

--- refume/app/controllers/users_controller.rb
+++ refume/app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      #log in user automatically once they finish signing up
+      log_in @user
       flash[:success] = "Welcome to RefuMe!"
       redirect_to @user
     else
