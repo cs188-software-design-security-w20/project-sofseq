495dd096a0d9c47b3985aad9e0a3b723afdd9ee4
wahjay <hotan38@berkeley.edu>
2020-02-13 11:35:44 -0800

    add bio column to users

--- refume/db/schema.rb
+++ refume/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210020058) do
+ActiveRecord::Schema.define(version: 20200213193422) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20200210020058) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "language"
+    t.text "bio"
   end
 
 end
