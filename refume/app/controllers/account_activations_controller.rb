765461be578f7848f0fbd0ce2d7d4c1d2902ec14
wahjay <hotan38@berkeley.edu>
2020-02-20 11:54:55 -0800

    integrating matching algorithm

--- refume/app/controllers/account_activations_controller.rb
+++ refume/app/controllers/account_activations_controller.rb
@@ -10,14 +10,46 @@ class AccountActivationsController < ApplicationController
       user.update_attribute(:activated_at, Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
-      #redirect user to pages based on their role selection
-      #implement later here
-      #redirect_to user
 
       #the matching algorithm should be called here
+      if user.role.downcase == 'mentee'
+        user_profile = []
+        user_profile << user.email
+        user_profile << user.country
+        user_profile << user.language
+        user_profile << user.goals
+        user_profile << user.bio
+        mentee = user_profile.join('|')
 
+        mentors_profile = User.where(role: 'mentor')
+        mentors = []
 
-      redirect_to matches_path
+        mentors_profile.each do |mentor|
+          mentor_profile = []
+          mentor_profile << mentor.email
+          mentor_profile << mentor.country
+          mentor_profile << mentor.language
+          mentor_profile << mentor.goals
+          mentor_profile << mentor.bio
+          mentors << mentor_profile.join('|')
+        end
+
+        # find match for this mentee
+        # It returns a list of mentors email
+        mentors_email = find_match(mentee, mentors)
+
+        # save the result to the database
+        # get the correspondingly mentors by email
+        mentors_email.each do |mentor_email|
+          mentor = User.where(email: mentor_email)
+          Match.new(mentor_id: mentor.id, mentee_id: user.id).save
+        end
+      end
+
+      #redirect user to pages based on their role selection
+      #implement later here
+      redirect_to user
+      #redirect_to matches_path
 
     else
       flash[:danger] = "Invalid activation link"
--- refume/app/helpers/account_activations_helper.rb
+++ refume/app/helpers/account_activations_helper.rb
@@ -1,2 +1,25 @@
 module AccountActivationsHelper
+  def get_matched_mentor_emails(stdout)
+      splits = stdout.split(',')
+      puts splits
+  end
+
+  # Need to implement this
+  def build_user_profile_as_string(profile)
+  end
+
+  def find_match(mentee, mentors)
+      query = ["python3", "matching_ruby_compatible.py"]
+      query.append(mentee)
+      mentors.each do |item|
+          query.append(item)
+      end
+      query.append(:err=>[:child, :out])
+      result = nil
+      IO.popen(query) {|ls_io|
+          result = ls_io.read
+      }
+      matches = get_matched_mentor_emails(result)
+      return matches
+  end
 end
