46bdca9635e0fd1138a8e4a04b14a13de0f38e78
Stewart Dulaney <sdulaney@users.noreply.github.com>
2020-03-20 05:28:03 -0700

    mark all cookies as SameSite=Strict

--- refume/app/controllers/application_controller.rb
+++ refume/app/controllers/application_controller.rb
@@ -5,3 +5,13 @@ class ApplicationController < ActionController::Base
   include MatchesHelper
   include AccountActivationsHelper
 end
+
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true, # mark all cookies as "Secure"
+    httponly: true, # mark all cookies as "HttpOnly"
+    samesite: {
+      strict: true # mark all cookies as SameSite=Strict
+    }
+  }
+end
